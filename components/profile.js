2b9cfd10e8df00049f3e1ca6b7361d344cc99376
Arturo Carrera <amerik0904@gmail.com>
2020-08-04 17:18:05 -0400

    profile

--- components/profile.js
+++ components/profile.js
@@ -1,15 +1,20 @@
 import React from "react";
-import { Form, Input, Button, Modal, Icon } from "antd";
-import Photo from './Photo.js';
-//import {graphql} from 'react-apollo';
-//import queries from '../utils/queries.js'
+import axios from 'axios';
+import { Form, Input, Button, Modal, Tooltip} from "antd";
+import {Image, Video, Transformation, CloudinaryContext} from 'cloudinary-react';
+
+const  url =  'https://api.cloudinary.com/v1_1/dqvbgvuoe/image/upload'
+const preset = 'imagesamerik'
 
 class ProfileForm extends React.Component {
     constructor(props) {
         super(props);     
         this.handleChange = this.handleChange.bind(this);
+        this.handleChangeFile = this.handleChangeFile.bind(this);
+       
         this.state = {
              visible: true,
+             disabled: false,
              formFields: {
                 firstName: "",
                 lastName: "",
@@ -19,41 +24,48 @@ class ProfileForm extends React.Component {
                 email: "",
                 linkedinAddress: "",
                 businessWebsite: "",
-
+                imageUrl: "",
+                publicId: "", 
              }
            
             };  
       }
 
-      getData(){
-        this.setState({ formFields: {firstName: "America" } })
-        this.setState({ formFields: {lastName: "Munoz" } })
-        this.setState({ formFields: {city: "Caracas" } })
-        this.setState({ formFields: {country: "Venezuela" } })
-        this.setState({ formFields: {email: "amerik0904@gmail.com" }  })
-        console.log("componentDidMount ", this.state.formFields)
-      }
-      componentDidMount(){
-        this.getData()
-      }
-      
-      handleSubmit = async (e) => {
-        e.preventDefault()
-        const values = JSON.stringify(this.state.formFields)
-        console.log("Formulario: ", this.state.formFields)
-        
-       /* const response = await this.mutation({
-            variables: this.state.formFields
-        })
-        console.log("Grapql response: ", response)*/
-      }
+    handleChangeFile = async (e) => {
+    const formData = new FormData();
+    formData.append('file', e.target.files[0]);
+    formData.append('upload_preset', preset);
 
-     handleChange(e) {
-        const { name, value } = e.target
+    try {
+        const res = await axios.post(url, formData);
+        console.log("cargue imagen en claudinary", res)   
+        
         this.setState({
-            formFields: { ...this.state.formFields, [name]: value }
+            formFields: { ...this.state.formFields, imageUrl: res.data.secure_url }
         })
-     }
+        this.setState({
+            formFields: { ...this.state.formFields, publicId: res.data.public_id }
+        })
+    } catch (err) {
+        console.error(err);
+    }     
+    }; 
+   
+    handleChange(e) {
+    const { name, value } = e.target
+    this.setState({
+        formFields: { ...this.state.formFields, [name]: value }
+    })
+    }
+
+    handleSubmit = async (e) => {
+    e.preventDefault()
+    const values = JSON.stringify(this.state.formFields)
+    console.log("Formulario: ", this.state.formFields)
+    
+    }
+      
+    
 
     render(){  
         const tailFormItemLayout = {
@@ -83,8 +95,12 @@ class ProfileForm extends React.Component {
                 phoneNumber, 
                 email, 
                 linkedinAddress, 
-                businessWebsite } = this.state.formFields
-
+                businessWebsite,
+                imageUrl,
+                publicId,
+                file,
+                value} = this.state.formFields
+        
         return(  
             
             <div>
@@ -99,7 +115,10 @@ class ProfileForm extends React.Component {
                     style={topModel}
                      > 
 
-                    <Form onSubmit={this.handleSubmit}>
+                    <Form 
+                        onSubmit={this.handleSubmit}
+                        >
+
                         <Form.Item label='First Name' style={widthStyle}>
                             <Input type="text" name="firstName"  placeholder="First Name" value={firstName} onChange={this.handleChange} />
                         </Form.Item>
@@ -125,13 +144,19 @@ class ProfileForm extends React.Component {
                         <Form.Item label='Business Website' style={widthStyle}>
                             <Input name="businessWebsite"  placeholder="Business Website" value={businessWebsite} onChange={this.handleChange}/>
                         </Form.Item>
-
-                        <Form.Item>
-                            <Photo />
+                        <Form.Item label='Profile Image' style={widthStyle}>
+                            <Input type="file" name="file"  onChange={this.handleChangeFile}/>
+                        </Form.Item>
+                        <Form.Item style={widthStyle}>
+                            <a href={this.state.formFields.imageUrl} target="_blank" >URL Image</a>
+                        </Form.Item>
+                        <Form.Item style={widthStyle}>
+                            <Image cloudName="dqvbgvuoe" publicId={publicId} width="100" crop="scale" />
                         </Form.Item>
 
                         <Form.Item {...tailFormItemLayout}>
-                            <Button
+                            <Button 
+                            
                                 style={{
                                     height: 40,
                                     width: "100%",
