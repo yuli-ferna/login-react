dca3ec7c19bb2bd674ca3d90c567726af3ad4741
yuli-ferna <yuliferna123@gmail.com>
2020-08-07 17:56:03 -0400

    cambio de funciones en NavBarUsers

--- components/navBarUsers.js
+++ components/navBarUsers.js
@@ -3,7 +3,8 @@ import { Menu, Dropdown, Button, Icon, message } from 'antd';
 import ProfileForm from './profile.js';
 import ProfilePng from './profilePng.js';
 import Gallery from './gallery.js';
-
+// import ControlAudio from './controlAudio/ControlAudio.js';
+// import SkyboxSelect from "./skybox-select";
 class NavBarUsers extends React.Component {
     constructor() {
         super();
@@ -11,48 +12,38 @@ class NavBarUsers extends React.Component {
           childVisible1: false,
           childVisible2: false,
           childVisible3: false,
+          childVisible4: false,
+          childVisible5: false,
+          childVisible6: false,
+          childVisible7: false,
+          childVisible8: false,
+
         }
       }
-    
-      onClick = ({ key }) => {
-        if (key == "1"){
-          this.setState({ childVisible1: true })
-          this.setState({ childVisible2: false })
-          this.setState({ childVisible3: false })
-        }
-        if (key == "2"){
-          this.setState({ childVisible1: false })
-          this.setState({ childVisible2: true })
-          this.setState({ childVisible3: false })
-        }
-        if (key == "3"){
-          this.setState({ childVisible1: false })
-          this.setState({ childVisible2: false })
-          this.setState({ childVisible3: true })
-        }
-      };  
+      
+    onClick = ({ key }) => {
+      this.setState({ [key]: true });
+    };  
 
-      closeChildVisible = ( key ) => {
-        if (key == "1"){
-          this.setState({ childVisible1: false })
-        }
-        if (key == "2"){
-          this.setState({ childVisible2: false })
-        }
-        if (key == "3"){
-          this.setState({ childVisible3: false })
-        }
-      };  
+    closeChildVisible = (key) => {
+    
+      this.setState({ [key]: false });
+    
+    }
 
     render(){
         const menu = (
             <Menu onClick={this.onClick} >
-                    <Menu.Item key="1">Profile</Menu.Item>
-                    <Menu.Item key="2" >Card</Menu.Item>
-                    <Menu.Item key="3">Gallery</Menu.Item> 
+                    <Menu.Item key="childVisible1">Profile</Menu.Item>
+                    <Menu.Item key="childVisible3">Gallery</Menu.Item> 
+                    <Menu.Item key="childVisible4">Audio</Menu.Item> 
+                    <Menu.Item key="childVisible5">Change Sky</Menu.Item> 
+                    <Menu.Item key="childVisible7">Change Scene</Menu.Item> 
+                    <Menu.Item key="childVisible6">Screenshare - Camera Test</Menu.Item> 
+                    <Menu.Item key="childVisible8">Logout</Menu.Item> 
             </Menu>
           );
-     
+
         return(
             <div>
                 <Dropdown overlay={menu} trigger={['click']} placement="bottomLeft" className='textAldrich'>
@@ -70,17 +61,33 @@ class NavBarUsers extends React.Component {
                     </Button>
                 </Dropdown>
 
+                
                 <div>
-                    
-                    {this.state.childVisible1 && <ProfileForm 
-                    close={this.closeChildVisible.bind(this, "1")}/> }
-
-                    {this.state.childVisible2 && <ProfilePng 
-                    close={this.closeChildVisible.bind(this,"2")}/> }
-
-                    {this.state.childVisible3 && <Gallery  
-                    close={this.closeChildVisible.bind(this,"3")}/> }
-                   
+                        <ProfileForm 
+                        visible={this.state.childVisible1 }
+                        close = {this.closeChildVisible.bind(this, 'childVisible1')}
+                        /> 
+                        {/* <ProfilePng 
+                        visible={this.state.childVisible2 }
+                        close = {this.closeChildVisible.bind(this, 'childVisible2')}
+                        />  */}
+                        <Gallery 
+                        visible={this.state.childVisible3 }
+                        close = {this.closeChildVisible.bind(this, 'childVisible3')}
+                        /> 
+                        <ControlAudio 
+                        visible={this.state.childVisible4 }
+                        close = {this.closeChildVisible.bind(this, 'childVisible4')}
+                        />
+                        <SkyboxSelect 
+                        visible={this.state.childVisible5 }
+                        close = {this.closeChildVisible.bind(this, 'childVisible5')}
+                        items={[16, 14, 22]}
+                        /> 
+                        {/* <ChangeScene 
+                        visible={this.state.childVisible6 }
+                        close = {this.closeChildVisible.bind(this, 'childVisible6')}
+                        />  */}
                 </div>
 
             </div>
