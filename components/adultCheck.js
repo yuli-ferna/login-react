a5614a61bba8929710babe22206a4023a705696a
Arturo Carrera <amerik0904@gmail.com>
2020-08-20 14:20:23 -0400

    adultCheck

--- components/adultCheck.js
+++ components/adultCheck.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
-import { Modal} from 'antd';
+import { Modal, Button} from 'antd';
 
 class AdultCheck extends Component {
-    state = { visible: true };
+  constructor(props) {
+    super(props);     
+    this.showModal = this.showModal.bind(this);
+ 
+    this.state = { visible: true,
+                   goToRef: false };
+    }
     
     showModal = () => {
       this.setState({
         visible: true,
       });
+      this.setState({
+        goToRef: true,
+      });
+    
     };
   
     hideModal = () => {
@@ -21,17 +31,24 @@ class AdultCheck extends Component {
           <Modal
               title="Age Confirmation"
               visible={this.state.visible}
+              footer={null}
               onOk={this.hideModal}
               onCancel={this.showModal}
               okText="Yes"
               cancelText="No"
-              
             >
+              
               <div style={{textAlign: "center"}}>
+                <p style={{color: "#ff0000", fontWeight: "bold"}}>ONLY USE GOOGLE CHROME BROWSER  LAPTOP / DESKTOP / OCULUS QUEST</p>
+                <br/>
                 <p>This website requires you to be 18 years or older to enter</p>
                 <h1>Are you over 18?</h1> 
               </div>
-             
+              <div style={{display: 'flex', justifyContent: "center"}}>
+                <Button type="primary" onClick={this.hideModal} style={{margin:"5px"}}>Si</Button>
+                <Button  style={{margin:"5px"}}
+                  href="https://kindling.burningman.org/" target="_blank">No</Button>
+              </div>
         </Modal>
         )
     }
